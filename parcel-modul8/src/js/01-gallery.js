7ab7ba19b6f9385e6d7871af0917e20ee9060d5e
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-02-03 22:13:35 +0200

    ex 1

--- parcel-modul8/src/js/01-gallery.js
+++ parcel-modul8/src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+
+const container = document.querySelector(".gallery");
+container.style.listStyleType = "none";
+const createList = galleryItems
+.map ( (a,idx,arr) => `<li class="gallery__item">
+                                        <a class="gallery__link" href="${a.original}">
+                                        <img class="gallery__image"
+                                            src="${a.preview}"
+                                            data-source="${a.original}"
+                                            alt="${a.description}"/>
+                                        </a>
+                                     </li>`)
+.join("");
+ container.insertAdjacentHTML("beforeend", createList);
+const displayList = new SimpleLightbox(".gallery a", {captionDelay : 200, captionsData : "alt" });
+
+
+
