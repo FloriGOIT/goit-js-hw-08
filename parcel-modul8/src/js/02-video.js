883a222852c2044598afe06f2c79907d4417d9c6
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-02-06 20:46:30 +0200

    ex 2

--- parcel-modul8/src/js/02-video.js
+++ parcel-modul8/src/js/02-video.js
@@ -1,1 +1,12 @@
 
+import Player from '@vimeo/player';
+
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', _.throttle(time => localStorage.setItem("key", time.seconds), 0));
+
+const resumeTime = localStorage.getItem("key");
+
+if (resumeTime != null) {player.setCurrentTime(resumeTime);}
